7510506abac0f44c0c6869eb5fc247ad9f854d8b
Shawn Almeida <shawn9920@gmail.com>
2023-12-17 03:30:10 +0530

    completed contact us, need to implement mailing system

--- pesino-pharma/app/contact/page.jsx
+++ pesino-pharma/app/contact/page.jsx
@@ -1,7 +1,137 @@
+import ContactForm from "@/components/contactform/ContactForm";
 import React from "react";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <div
+        className="relative bg-cover bg-center flex"
+        style={{ height: "auto" }}
+      >
+        {/* <!-- Background Image --> */}
+        <div className="absolute inset-0 bg-cover bg-center bg-banner-manu"></div>{" "}
+        {/* <!-- Gradient Overlay --> */}
+        <div className="absolute inset-0 bg-gradient-to-bl from-transparent to-blue-900"></div>
+        {/* <!-- Your content goes here --> */}
+        <div className="flex flex-col">
+          <div className="relative flex-col flex items-center justify-center h-full text-white px-4 ">
+            <div className="flex flex-col w-full h-full justify-start items-center gap-8 my-32">
+              <div className="text-white font-medium text-6xl">CONTACT US</div>
+              <div className="w-2/3 flex justify-center font-light text-3xl text-white">
+                <p>
+                  Discover high-quality pharmaceutical imports with Pesino
+                  Pharma, an esteemed Indian manufacturer and exporter. We
+                  specialize in importing premium raw materials and components
+                  to ensure the excellence of our pharmaceutical products. Our
+                  commitment to strict quality control, adherence to
+                  international standards, and rigorous testing procedures sets
+                  us apart.
+                </p>
+              </div>
+            </div>
+            <div className="bg-white flex my-32 ">
+              <div className="flex flex-row w-full text-black">
+                <div className="my-16 p-4 flex justify-center gap-40">
+                  <div className="left_side flex flex-col w-1/2 px-10 ml-8">
+                    <div className="themeColor text-xl my-1 flex">
+                      Contact Us
+                    </div>
+                    <div className="text-4xl font-semibold my-4  ">
+                      Get In Touch
+                    </div>
+                    <div className="text-xl">
+                      <p>
+                        Explore our comprehensive pharmaceutical solutions and
+                        experience the commitment to quality that defines Pesino
+                        Pharma. Contact us today to discuss how we can meet your
+                        specific requirements and contribute to your success in
+                        the healthcare industry.
+                      </p>
+                    </div>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4 my-8">
+                      <div className="p-4 flex flex-col">
+                        <h1 className="text-2xl my-2 font-semibold">
+                          Phone Number
+                        </h1>
+                        <h1 className="text-xl font-normal">+91 9930992024</h1>
+                      </div>
+
+                      <div className="p-4">
+                        <h1 className="text-2xl my-2 font-semibold">
+                          Email address
+                        </h1>
+                        <h1 className="text-xl font-normal">
+                          pesino_pharma@yahoo.co.in
+                        </h1>
+                      </div>
+
+                      <div className=" p-4">
+                        <h1 className="text-2xl my-2 font-semibold">Website</h1>
+                        <h1 className="text-xl font-normal">
+                          www.pesino-pharma.com
+                        </h1>
+                      </div>
+                      <div className=" p-4">
+                        <h1 className="text-2xl my-2 font-semibold">Address</h1>
+                        <h1 className="text-xl font-normal">
+                          11/B-405, Ashish Complex, Dahisar East, MUMBAI-400068,
+                          INDIA.
+                        </h1>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="right_side flex flex-col w-1/2 mr-8">
+                    <div className="border-2 shadow-md rounded-3xl bg-blue-700">
+                      <div className="bg-white flex w-full mt-8 p-4">
+                        <div className="flex flex-col p-8 justify-around w-full gap-10">
+                          {/* <form
+                            onSubmit={handleSubmit}
+                            // className="flex flex-col items-center"
+                          >
+                            <div className="flex flex-row justify-start gap-16">
+                              <div className="flex flex-col">
+                                <label className="mb-1">First Name</label>
+                                <input
+                                  className="border-2 border-solid border-blue-500 p-2 rounded-lg"
+                                  placeholder="First Name"
+                                />
+                              </div>
+                              <div className="flex flex-col">
+                                <label className="mb-1">Last Name</label>
+                                <input
+                                  className="border-2 border-solid border-blue-500 p-2 rounded-lg"
+                                  placeholder="Last Name"
+                                />
+                              </div>
+                            </div>
+                          </form> */}
+                          <ContactForm />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-end flex-col my-32 gap-8">
+              <div className="text-white text-center font-medium text-6xl my-4">
+                Consult us for the Meds you need now!
+              </div>
+              <div className="w-full flex justify-center text-center font-light text-3xl text-white">
+                <p className="w-2/3">
+                  Whether you are looking for specific medications,
+                  collaborations, or have inquiries, our dedicated team is ready
+                  to assist you. Consult Pesino Pharma for the meds you need,
+                  and experience excellence in pharmaceutical solutions. Your
+                  health is our priority!
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
---
+++ pesino-pharma/components/contactform/ContactForm.jsx
@@ -0,0 +1,120 @@
+"use client";
+import React, { useState } from "react";
+
+function ContactForm() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [formData, setFormData] = useState({
+    Fname: "",
+    Lname: "",
+    email: "",
+    message: "",
+  });
+  const [isButtonAnimated, setIsButtonAnimated] = useState(false);
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  const handleMouseMove = (e) => {
+    setCursorPosition({ x: e.clientX, y: e.clientY });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validate form fields
+    if (
+      formData.Fname === "" ||
+      formData.Lname === "" ||
+      formData.email === "" ||
+      formData.message === ""
+    ) {
+      alert("Please fill in all fields!");
+      setIsButtonAnimated(true);
+      return;
+    }
+
+    // Perform submission logic here (e.g., send data to the server)
+
+    setIsSubmitting(true);
+
+    // Simulate API call (you would replace this with your actual submission logic)
+    setTimeout(() => {
+      alert("Form submitted successfully!");
+      setIsSubmitting(false);
+      setIsButtonAnimated(false);
+    }, 1000);
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      onMouseMove={handleMouseMove}
+      // className="flex flex-col items-center"
+    >
+      <div className="flex flex-row justify-start gap-16">
+        <div className="flex flex-col">
+          <label className="mb-1">First Name</label>
+          <input
+            type="text"
+            className="border-2 border-solid border-blue-500 p-2 rounded-lg"
+            placeholder="First Name"
+            name="Fname"
+            value={formData.Fname}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="mb-1">Last Name</label>
+          <input
+            type="text"
+            className="border-2 border-solid border-blue-500 p-2 rounded-lg"
+            placeholder="Last Name"
+            name="Lname"
+            value={formData.Lname}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <label className="mb-1">Email-id</label>
+        <input
+          type="email"
+          className="border-2 border-solid border-blue-500 p-2 rounded-lg"
+          placeholder="Email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="flex flex-col">
+        <label className="mb-1">Message</label>
+        <textarea
+          className="border-2 border-solid border-blue-500 p-2 rounded-lg"
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="flex my-8 w-full">
+        <button
+          type="submit"
+          className={`bg-blue-500 text-white px-4 py-2 transition-transform duration-300 ${
+            isSubmitting
+              ? "transform translate-x-20"
+              : isButtonAnimated
+              ? `transform translate-x-${cursorPosition.x / 10}`
+              : ""
+          }`}
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? "Submitting..." : "Submit"}
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default ContactForm;
