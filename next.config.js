120150e2a753e885d76a13af641f044a1190a274
Shawn Almeida <shawn9920@gmail.com>
2023-12-20 17:19:24 +0530

    smol changes

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
     loader: "default",
-    path: "https://noop/",
+    path: "",
     remotePatterns: [
       {
         protocol: "https",
